3073df2bfd3d5f53b0987788603ae35f9c1c28f8
thales santos <thales.infor144@gmail.com>
2025-04-02 14:46:55 -0300

    fix: fix the logout return

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/login');
+        return view('auth.login');
     }
 
     // Registro
