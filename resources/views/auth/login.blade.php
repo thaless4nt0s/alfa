1683dddf5a0b72d63dc08303c0ee2c343ea87736
thales santos <thales.infor144@gmail.com>
2025-04-02 16:52:30 -0300

    fix: Change the name in portuguese to english

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                     <input type="email" name="email" class="w-full px-3 py-2 border rounded">
                 </div>
                 <div class="mb-4">
-                    <label class="block text-gray-700 mb-2">Senha</label>
+                    <label class="block text-gray-700 mb-2">Password</label>
                     <input type="password" name="password" class="w-full px-3 py-2 border rounded">
                 </div>
                 <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
