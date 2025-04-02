6aaa0e5cea23a14c57e54c169abc603c10fe3033
thales santos <thales.infor144@gmail.com>
2025-04-02 14:47:22 -0300

    feat: Include the Contact's Model

---
+++ app/Models/Contact.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'contact'
+    ];
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:6',
+            'contact' => 'required|digits:9',
+            'email' => 'required|email|unique:contacts'
+        ];
+    }
+}
