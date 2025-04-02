47f3dc955ce7044919ae5d1c7fee95d722451a8d
thales santos <thales.infor144@gmail.com>
2025-04-02 15:12:48 -0300

    feat: Include the routes to update a contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     protected $contact;
+
     public function __construct(Contact $contact)
     {
         $this->contact = $contact;
@@ -26,12 +27,31 @@ class ContactController extends Controller
 
         Contact::create($validated);
 
-        return redirect()->route('dashboard')->with('success', 'Contact has been created !');
+        return redirect()->route('dashboard')->with('success', 'Contact has been created!');
     }
 
     public function index()
     {
         $contacts = $this->contact->select('id', 'name', 'email', 'contact')->get();
         return view('dashboard', compact('contacts'));
+    }
+
+    // Atualizar contato
+    public function edit($id)
+    {
+        $contact = $this->contact->findOrFail($id);
+        return view('contacts.edit', compact('contact'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate(
+            $this->contact->rules()
+        );
+
+        $contact = $this->contact->findOrFail($id);
+        $contact->update($validated);
+
+        return redirect()->route('dashboard')->with('success', 'Contact has been updated!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,6 @@ Route::prefix('app')->middleware('auth')->group(function () {
     Route::get('/dashboard', [ContactController::class, 'index'])->name('dashboard');
     Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
     Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
 });
